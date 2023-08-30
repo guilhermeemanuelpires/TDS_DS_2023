ecff9ee261599e2281acccf2a90b1f5aed1c43ef
User <guilhermeemanuel45@gmail.com>
2023-08-30 14:06:43 -0300

    projeto lista mercado

---
+++ lista_mercado/js/index.js
@@ -0,0 +1,15 @@
+var a = 2;
+
+
+// var b = a == 2 ? "Hoje tem aula" : "Amanha tem aula";
+var b = "";
+
+if(a === 2){
+    b = "Hoje tem aula";
+}else{
+    b = "Amanha tem aula";
+}
+
+console.log(b);
+
+
