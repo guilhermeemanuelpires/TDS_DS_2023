cdca721167354e4189c1bbc05d695061f8f0e29f
User <guilhermeemanuel45@gmail.com>
2023-10-18 14:40:11 -0300

    script tabela turmas

--- api/scripts.sql
+++ api/scripts.sql
@@ -8,12 +8,22 @@ CREATE TABLE `alunos` (
   PRIMARY KEY (`id`)
 );
 
-
 CREATE TABLE `professor` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `materia` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `turmas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `professor_id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `turmas_FK` (`professor_id`),
+  KEY `turmas_FK_1` (`aluno_id`),
+  CONSTRAINT `turmas_FK` FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `turmas_FK_1` FOREIGN KEY (`aluno_id`) REFERENCES `alunos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO apiDB.alunos (id, nome, idade, email) VALUES(1, 'Laizia', 17, NULL);
